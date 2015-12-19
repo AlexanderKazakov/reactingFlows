b7762c266258703f426be22b2a79d6cc68fee3e8
AlexanderKazakov <alexanderkazak@yandex.ru>
2015-12-19 14:18:17 +0300

    more real-life situation

--- src/EquationSystem.cpp
+++ src/EquationSystem.cpp
@@ -29,22 +29,22 @@ double EquationSystem::residualError(const double* u) const {
 
 double EquationSystem::getValue(const int i, const double* u) const {
 	switch (implementation) {
-//		case GAS_DETONATION:
-//			return static_cast<const GasDetonationSystem*>(this)->getValue(i, u);
+		case GAS_DETONATION:
+			return static_cast<const GasDetonationSystem*>(this)->getValue(i, u);
 		case IMPLICIT_EULER_METHOD:
 			return static_cast<const ImplicitEulerMethodSystem*>(this)->getValue(i, u);
-//		case PLOT_HUGONIOT_ISENTROPIC:
-//			return static_cast<const SystemToPlotHugoniotIsentropic*>(this)->getValue(i, u);
-//		case PLOT_SHOCK_ISENTROPIC:
-//			return static_cast<const SystemToPlotShockIsentropic*>(this)->getValue(i, u);
+		case PLOT_HUGONIOT_ISENTROPIC:
+			return static_cast<const SystemToPlotHugoniotIsentropic*>(this)->getValue(i, u);
+		case PLOT_SHOCK_ISENTROPIC:
+			return static_cast<const SystemToPlotShockIsentropic*>(this)->getValue(i, u);
 		case ZELDOVICH:
 			return static_cast<const ZeldovichSystem*>(this)->getValue(i, u);
-//		case LINEAR_EQUATION:
-//			return static_cast<const LinearEquationSystem*>(this)->getValue(i, u);
-//		case NON_LINEAR_EQUATION:
-//			return static_cast<const NonLinearEquationSystem*>(this)->getValue(i, u);
-//		case TEST_ODE:
-//			return static_cast<const TestODESystem*>(this)->getValue(i, u);
+		case LINEAR_EQUATION:
+			return static_cast<const LinearEquationSystem*>(this)->getValue(i, u);
+		case NON_LINEAR_EQUATION:
+			return static_cast<const NonLinearEquationSystem*>(this)->getValue(i, u);
+		case TEST_ODE:
+			return static_cast<const TestODESystem*>(this)->getValue(i, u);
 		default:
 			throw "No such implementation";
 	}
@@ -52,22 +52,22 @@ double EquationSystem::getValue(const int i, const double* u) const {
 
 double EquationSystem::getDerivative(const int i, const int j, const double* u) const {
 	switch (implementation) {
-//		case GAS_DETONATION:
-//			return static_cast<const GasDetonationSystem*>(this)->getDerivative(i, j, u);
+		case GAS_DETONATION:
+			return static_cast<const GasDetonationSystem*>(this)->getDerivative(i, j, u);
 		case IMPLICIT_EULER_METHOD:
 			return static_cast<const ImplicitEulerMethodSystem*>(this)->getDerivative(i, j, u);
-//		case PLOT_HUGONIOT_ISENTROPIC:
-//			return static_cast<const SystemToPlotHugoniotIsentropic*>(this)->getDerivative(i, j, u);
-//		case PLOT_SHOCK_ISENTROPIC:
-//			return static_cast<const SystemToPlotShockIsentropic*>(this)->getDerivative(i, j, u);
+		case PLOT_HUGONIOT_ISENTROPIC:
+			return static_cast<const SystemToPlotHugoniotIsentropic*>(this)->getDerivative(i, j, u);
+		case PLOT_SHOCK_ISENTROPIC:
+			return static_cast<const SystemToPlotShockIsentropic*>(this)->getDerivative(i, j, u);
 		case ZELDOVICH:
 			return static_cast<const ZeldovichSystem*>(this)->getDerivative(i, j, u);
-//		case LINEAR_EQUATION:
-//			return static_cast<const LinearEquationSystem*>(this)->getDerivative(i, j, u);
-//		case NON_LINEAR_EQUATION:
-//			return static_cast<const NonLinearEquationSystem*>(this)->getDerivative(i, j, u);
-//		case TEST_ODE:
-//			return static_cast<const TestODESystem*>(this)->getDerivative(i, j, u);
+		case LINEAR_EQUATION:
+			return static_cast<const LinearEquationSystem*>(this)->getDerivative(i, j, u);
+		case NON_LINEAR_EQUATION:
+			return static_cast<const NonLinearEquationSystem*>(this)->getDerivative(i, j, u);
+		case TEST_ODE:
+			return static_cast<const TestODESystem*>(this)->getDerivative(i, j, u);
 		default:
 			throw "No such implementation";
 	}
@@ -75,22 +75,22 @@ double EquationSystem::getDerivative(const int i, const int j, const double* u) 
 
 void EquationSystem::getFirstApproximation(double* u) const {
 	switch (implementation) {
-//		case GAS_DETONATION:
-//			return static_cast<const GasDetonationSystem*>(this)->getFirstApproximation(u);
+		case GAS_DETONATION:
+			return static_cast<const GasDetonationSystem*>(this)->getFirstApproximation(u);
 		case IMPLICIT_EULER_METHOD:
 			return static_cast<const ImplicitEulerMethodSystem*>(this)->getFirstApproximation(u);
-//		case PLOT_HUGONIOT_ISENTROPIC:
-//			return static_cast<const SystemToPlotHugoniotIsentropic*>(this)->getFirstApproximation(u);
-//		case PLOT_SHOCK_ISENTROPIC:
-//			return static_cast<const SystemToPlotShockIsentropic*>(this)->getFirstApproximation(u);
+		case PLOT_HUGONIOT_ISENTROPIC:
+			return static_cast<const SystemToPlotHugoniotIsentropic*>(this)->getFirstApproximation(u);
+		case PLOT_SHOCK_ISENTROPIC:
+			return static_cast<const SystemToPlotShockIsentropic*>(this)->getFirstApproximation(u);
 		case ZELDOVICH:
 			return static_cast<const ZeldovichSystem*>(this)->getFirstApproximation(u);
-//		case LINEAR_EQUATION:
-//			return static_cast<const LinearEquationSystem*>(this)->getFirstApproximation(u);
-//		case NON_LINEAR_EQUATION:
-//			return static_cast<const NonLinearEquationSystem*>(this)->getFirstApproximation(u);
-//		case TEST_ODE:
-//			return static_cast<const TestODESystem*>(this)->getFirstApproximation(u);
+		case LINEAR_EQUATION:
+			return static_cast<const LinearEquationSystem*>(this)->getFirstApproximation(u);
+		case NON_LINEAR_EQUATION:
+			return static_cast<const NonLinearEquationSystem*>(this)->getFirstApproximation(u);
+		case TEST_ODE:
+			return static_cast<const TestODESystem*>(this)->getFirstApproximation(u);
 		default:
 			throw "No such implementation";
 	}
@@ -98,22 +98,22 @@ void EquationSystem::getFirstApproximation(double* u) const {
 
 void EquationSystem::printCompleteSolution(const double* u) const {
 	switch (implementation) {
-//		case GAS_DETONATION:
-//			return static_cast<const GasDetonationSystem*>(this)->printCompleteSolution(u);
+		case GAS_DETONATION:
+			return static_cast<const GasDetonationSystem*>(this)->printCompleteSolution(u);
 		case IMPLICIT_EULER_METHOD:
 			return static_cast<const ImplicitEulerMethodSystem*>(this)->printCompleteSolution(u);
-//		case PLOT_HUGONIOT_ISENTROPIC:
-//			return static_cast<const SystemToPlotHugoniotIsentropic*>(this)->printCompleteSolution(u);
-//		case PLOT_SHOCK_ISENTROPIC:
-//			return static_cast<const SystemToPlotShockIsentropic*>(this)->printCompleteSolution(u);
+		case PLOT_HUGONIOT_ISENTROPIC:
+			return static_cast<const SystemToPlotHugoniotIsentropic*>(this)->printCompleteSolution(u);
+		case PLOT_SHOCK_ISENTROPIC:
+			return static_cast<const SystemToPlotShockIsentropic*>(this)->printCompleteSolution(u);
 		case ZELDOVICH:
 			return static_cast<const ZeldovichSystem*>(this)->printCompleteSolution(u);
-//		case LINEAR_EQUATION:
-//			return static_cast<const LinearEquationSystem*>(this)->printCompleteSolution(u);
-//		case NON_LINEAR_EQUATION:
-//			return static_cast<const NonLinearEquationSystem*>(this)->printCompleteSolution(u);
-//		case TEST_ODE:
-//			return static_cast<const TestODESystem*>(this)->printCompleteSolution(u);
+		case LINEAR_EQUATION:
+			return static_cast<const LinearEquationSystem*>(this)->printCompleteSolution(u);
+		case NON_LINEAR_EQUATION:
+			return static_cast<const NonLinearEquationSystem*>(this)->printCompleteSolution(u);
+		case TEST_ODE:
+			return static_cast<const TestODESystem*>(this)->printCompleteSolution(u);
 		default:
 			throw "No such implementation";
 	}
